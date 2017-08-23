2bc04c363a5afafef0e22164eea1f58a4f360ca0
HUYLE <lehuy.paul@gmail.com>
2017-08-23 09:22:00 +0700

    update API updateStatus

--- devices/updateStatus.js
+++ devices/updateStatus.js
@@ -7,82 +7,44 @@ const constants = require('../lib/constants');
 
 module.exports.updateStatus = (event, context, callback) => {
 
-  const timestamp = new Date().getTime();
   const input = JSON.parse(event.body);
-  const userId = event.requestContext.authorizer.principalId;
-  var userRole = utils.decodedToken(event.headers.Authorization.substring(4));
-  const getParams = {
+  const params = {
     TableName: process.env.DEVICES_TABLE_NAME,
-    Key: {
-      id: event.pathParameters.id,
-      ownerId: userId,
-    }
+    IndexName: 'DevicesChipIdIndex',
+    KeyConditionExpression: 'chipId = :chipId',
+    ExpressionAttributeValues: { ':chipId': input.chipId }
   };
 
-  dynamodb.get(getParams, (error, result) => {
-    if (error) {
-      console.error(error);
-      callback(null, utils.createResponse(500, 'An internal server error occurred '));
+  dynamodb.query(params, function (err, result) {
+    if (err) {
+      console.log(err);
+      callback(null, utils.createResponse(500, 'An internal server error occurred'));
       return;
     }
+    var attributeUpdates = utils.toAttributeUpdates({
+      status: constants.STATUS_OFFLINE,
+      updatedAt: new Date().getTime()
+    });
+    var result = JSON.stringify(result.Items);
+    var result = result.slice(1,result.length-1);
+    var result = JSON.parse(result);
+     
+    dynamodb.update({
+      TableName: process.env.DEVICES_TABLE_NAME,
+      AttributeUpdates: attributeUpdates,
+      Key: {
+        id: result.id,
+        ownerId: result.ownerId
+      },
+      ReturnValues: 'ALL_NEW'
+    }, (error, result) => {
+      if (error) {
+        console.error(error);
+        callback(null, utils.createResponse(500, 'An internal server error occurred'));
+        return;
+      }
 
-    if (_.isEmpty(result)) {
-      callback(null, utils.createResponse(400, 'Device not exist'));
-      return;
-    }
-
-    if (result.Item.chipId != input.chipId) {
-      console.error(error);
-      callback(null, utils.createResponse(500, 'Invalid chipId '));
-      return;
-    }
-
-    if (userRole == constants.ROLE_BROKER) {
-      var attributeUpdates = utils.toAttributeUpdates({
-        status: constants.STATUS_OFFLINE,
-        updatedAt: new Date().getTime()
-      });
-      dynamodb.update({
-        TableName: process.env.DEVICES_TABLE_NAME,
-        AttributeUpdates: attributeUpdates,
-        Key: {
-          id: event.pathParameters.id,
-          ownerId: userId
-        },
-        ReturnValues: 'ALL_NEW'
-      }, (error, result) => {
-        if (error) {
-          console.error(error);
-          callback(null, utils.createResponse(500, 'An internal server error occurred'));
-          return;
-        }
-
-        callback(null, utils.createResponse(200, null, result.Attributes));
-      });
-    }
-    else {
-      var attributeUpdates = utils.toAttributeUpdates({
-        status: input.status,
-        updatedAt: new Date().getTime()
-      });
-
-      dynamodb.update({
-        TableName: process.env.DEVICES_TABLE_NAME,
-        AttributeUpdates: attributeUpdates,
-        Key: {
-          id: event.pathParameters.id,
-          ownerId: userId
-        },
-        ReturnValues: 'ALL_NEW'
-      }, (error, result) => {
-        if (error) {
-          console.error(error);
-          callback(null, utils.createResponse(500, 'An internal server error occurred'));
-          return;
-        }
-
-        callback(null, utils.createResponse(200, null, result.Attributes));
-      });
-    }
+      callback(null, utils.createResponse(200, null, result.Attributes));
+    });
   });
 }
