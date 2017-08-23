1999eef7d9d5d18fed49e7208586da0950ecc909
HUYLE <lehuy.paul@gmail.com>
2017-08-23 17:05:00 +0700

    update create message

--- messages/create.js
+++ messages/create.js
@@ -1,44 +1,39 @@
 'use strict';
 
 const uuid = require('uuid');
-const shortid = require('shortid');
-const validator = require('validator');
-const bcrypt = require('bcrypt-nodejs');
-const jwt = require('jsonwebtoken');
-const config = require('../lib/config');
 const dynamodb = require('../lib/dynamodb');
 const utils = require('../lib/utils');
 
 module.exports.create = (event, context, callback) => {
+  
   const timestamp = new Date().getTime();
   var input = JSON.parse(event.body);
   const userId = event.requestContext.authorizer.principalId;
-  
-  if (!input.topic ) {
-    callback(null, utils.createResponse(400, 'Invalid topic'));
+  if (!input.topic) {
+    callback(null, utils.createResponse(400, 'Invalid topic name '));
     return;
   }
 
   var params = {
     TableName: process.env.MESSAGES_TABLE_NAME,
-    Item: {
+    Item: utils.filterBlankAttributes( {
       userId: userId,
       topic: input.topic,
       data : input.data,
       clientId : input.chipId,
       createdAt: timestamp,
       updatedAt: timestamp,
-    },
+    }),
   };
 
   dynamodb.put(params, (error) => {
     if (error) {
       console.error(error);
-      callback(null, utils.createResponse(500, 'An internal server error occurred'));
+      callback(null, utils.createResponse(500));
       return;
     }
 
-    callback(null, utils.createResponse(200,null, params.Item));
+    callback(null, utils.createResponse(200, null, params.Item));
   });
 };
 
