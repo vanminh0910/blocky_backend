2d077bcbc77f82f8286a6d21b32b352e968e5db3
huyle0110 <lehuy.paul@gmail.com>
2017-08-24 09:14:26 +0700

    test API create a new message (#6)
    Add API to create a new message

--- messages/create.js
+++ messages/create.js
@@ -1,1 +1,39 @@
+'use strict';
+
+const uuid = require('uuid');
+const dynamodb = require('../lib/dynamodb');
+const utils = require('../lib/utils');
+
+module.exports.create = (event, context, callback) => {
+  
+  const timestamp = new Date().getTime();
+  var input = JSON.parse(event.body);
+  const userId = event.requestContext.authorizer.principalId;
+  if (!input.topic) {
+    callback(null, utils.createResponse(400, 'Invalid topic name '));
+    return;
+  }
+
+  var params = {
+    TableName: process.env.MESSAGES_TABLE_NAME,
+    Item: utils.filterBlankAttributes( {
+      userId: userId,
+      topic: input.topic,
+      data : input.data,
+      clientId : input.chipId,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    }),
+  };
+
+  dynamodb.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, utils.createResponse(500));
+      return;
+    }
+
+    callback(null, utils.createResponse(200, null, params.Item));
+  });
+};
 
