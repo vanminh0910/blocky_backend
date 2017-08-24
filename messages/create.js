2ae1f9fce18b0969fcc03378cd688b5b411d9c2a
Minh Ha <vanminh0910@gmail.com>
2017-08-24 10:27:20 +0700

    Correct API to create message

--- messages/create.js
+++ messages/create.js
@@ -8,32 +8,52 @@ module.exports.create = (event, context, callback) => {
   
   const timestamp = new Date().getTime();
   var input = JSON.parse(event.body);
-  const userId = event.requestContext.authorizer.principalId;
-  if (!input.topic) {
-    callback(null, utils.createResponse(400, 'Invalid topic name '));
+
+  if (!input.topic || !input.authKey) {
+    callback(null, utils.createResponse(400, 'Invalid message data'));
     return;
   }
 
+  // lookup auth key to find user
   var params = {
-    TableName: process.env.MESSAGES_TABLE_NAME,
-    Item: utils.filterBlankAttributes( {
-      userId: userId,
-      topic: input.topic,
-      data : input.data,
-      clientId : input.chipId,
-      createdAt: timestamp,
-      updatedAt: timestamp,
-    }),
+    TableName: process.env.USERS_TABLE_NAME,
+    IndexName: 'UsersAuthKeyIndex',
+    KeyConditionExpression: 'authKey = :authKey',
+    ExpressionAttributeValues: {':authKey': input.authKey}
   };
 
-  dynamodb.put(params, (error) => {
-    if (error) {
-      console.error(error);
+  dynamodb.query(params, function(err, result) {
+    if (err) {
+      console.log(err);
       callback(null, utils.createResponse(500));
       return;
-    }
+    } else if (result.Count != 1) {
+      callback(null, utils.createResponse(404, 'Authentication key not found'));
+    } else {
+      var user = result.Items[0];
 
-    callback(null, utils.createResponse(200, null, params.Item));
+      var params = {
+        TableName: process.env.MESSAGES_TABLE_NAME,
+        Item: {
+          id: uuid.v1(),
+          userId: user.id,
+          topic: input.topic,
+          data : input.data || ' ',
+          createdAt: timestamp,
+          updatedAt: timestamp,
+        },
+      };
+
+      dynamodb.put(params, (error) => {
+        if (error) {
+          console.error(error);
+          callback(null, utils.createResponse(500));
+          return;
+        }
+
+        callback(null, utils.createResponse(200, null, params.Item));
+      });  
+    }
   });
 };
 
